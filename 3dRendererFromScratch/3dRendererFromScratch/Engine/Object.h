a90898344d7283714a942b8d8c8cc6237638b57b
Alexander Smorodinov <asmorodinov66@gmail.com>
2020-11-10 00:25:51 +0300

    Немного поменял .gitignore

--- 3dRendererFromScratch/3dRendererFromScratch/Engine/Object.h
+++ 3dRendererFromScratch/3dRendererFromScratch/Engine/Object.h
@@ -18,7 +18,7 @@ class Object {
     virtual void draw(const Camera& camera, Screen& screen) = 0;
     virtual void update(float dt) = 0;
 
-    Transform& getTransform() { return t; }
+    Transform& getTransform();
 
  protected:
     Transform t;
@@ -28,29 +28,10 @@ using ObjectPtr = std::unique_ptr<Object>;
 
 class Point : public Object {
  public:
-    Point(glm::vec3 position, ColorType color = {1.0f, 1.0f, 1.0f}, unsigned pixelRadius = 1)
-        : color(color), pixelRadius(pixelRadius) {
-        t.position = position;
-    }
+    Point(glm::vec3 position, ColorType color = {1.0f, 1.0f, 1.0f}, unsigned pixelRadius = 1);
 
-    void draw(const Camera& camera, Screen& screen) override {
-        glm::vec2 screenCoords = LocalCoordsToScreenCoords(camera.getViewMatrix(), screen.getProjectionMatrix(),
-                                                           t.getModel(), glm::vec3(0.0f));
-
-        if (screenCoords.x <= -1.0f || screenCoords.x >= 1.0f || screenCoords.y <= -1.0f || screenCoords.y >= 1.0f)
-            return;
-
-        int x = int(screen.getWidth() * (screenCoords.x + 1.0f) / 2.0f);
-        int y = int(screen.getHeight() * (screenCoords.y + 1.0f) / 2.0f);
-
-        for (int i = x - pixelRadius; i <= x + pixelRadius; ++i)
-            for (int j = y - pixelRadius; j <= y + pixelRadius; ++j) {
-                if (i < 0 || j < 0) continue;
-
-                screen.setPixelColor(size_t(i), size_t(j), color);
-            }
-    }
-    void update(float dt) override{};
+    void draw(const Camera& camera, Screen& screen) override;
+    void update(float dt) override;
 
  private:
     ColorType color;
--- 3dRendererFromScratch/3dRendererFromScratch/Engine/Renderer.h
+++ 3dRendererFromScratch/3dRendererFromScratch/Engine/Renderer.h
@@ -16,31 +16,12 @@ class Renderer {
  public:
     Renderer(size_t width, size_t height);
 
-    Screen& getScreen() { return screen; }
-    World& getWorld() { return world; }
+    Screen& getScreen();
+    World& getWorld();
 
-    void renderSceneToScreen() {
-        for (auto& object : world.getObjects()) object->draw(world.getCamera(), screen);
-    }
+    void renderSceneToScreen();
 
-    void renderScreenToFile(const std::string& file) const {
-        size_t width = screen.getWidth();
-        size_t height = screen.getHeight();
-
-        std::vector<uint8_t> pixels(width * height * 3, 0);
-
-        for (size_t x = 0; x < width; ++x) {
-            for (size_t y = 0; y < height; ++y) {
-                ColorType color = screen.getPixelColor(x, y);
-
-                pixels[3 * (y * width + x) + 0] = uint8_t(255.99f * color.r);
-                pixels[3 * (y * width + x) + 1] = uint8_t(255.99f * color.g);
-                pixels[3 * (y * width + x) + 2] = uint8_t(255.99f * color.b);
-            }
-        }
-
-        stbi_write_png(file.c_str(), width, height, 3, &pixels[0], width * 3);
-    }
+    void renderScreenToFile(const std::string& file) const;
 
  private:
     Screen screen;
