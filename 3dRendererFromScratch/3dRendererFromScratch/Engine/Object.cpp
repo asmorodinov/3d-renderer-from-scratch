afbae9b44fe22f78625f944c5980a8c92256406a
Alexander Smorodinov <asmorodinov66@gmail.com>
2020-11-10 00:25:51 +0300

    Немного поменял .gitignore

--- 3dRendererFromScratch/3dRendererFromScratch/Engine/Object.cpp
+++ 3dRendererFromScratch/3dRendererFromScratch/Engine/Object.cpp
@@ -1,3 +1,30 @@
 #include "Object.h"
 
-namespace eng {}  // namespace eng
+namespace eng {
+
+Transform& Object::getTransform() { return t; }
+
+Point::Point(glm::vec3 position, ColorType color, unsigned pixelRadius) : color(color), pixelRadius(pixelRadius) {
+    t.position = position;
+}
+
+void Point::draw(const Camera& camera, Screen& screen) {
+    glm::vec2 screenCoords =
+        LocalCoordsToScreenCoords(camera.getViewMatrix(), screen.getProjectionMatrix(), t.getModel(), glm::vec3(0.0f));
+
+    if (screenCoords.x <= -1.0f || screenCoords.x >= 1.0f || screenCoords.y <= -1.0f || screenCoords.y >= 1.0f) return;
+
+    int x = int(screen.getWidth() * (screenCoords.x + 1.0f) / 2.0f);
+    int y = int(screen.getHeight() * (screenCoords.y + 1.0f) / 2.0f);
+
+    for (int i = x - pixelRadius; i <= x + pixelRadius; ++i)
+        for (int j = y - pixelRadius; j <= y + pixelRadius; ++j) {
+            if (i < 0 || j < 0) continue;
+
+            screen.setPixelColor(size_t(i), size_t(j), color);
+        }
+}
+
+void Point::update(float dt) {}
+
+}  // namespace eng
--- 3dRendererFromScratch/3dRendererFromScratch/Engine/Renderer.cpp
+++ 3dRendererFromScratch/3dRendererFromScratch/Engine/Renderer.cpp
@@ -4,4 +4,30 @@ namespace eng {
 
 Renderer::Renderer(size_t width, size_t height) : screen(width, height, {0.3f, 0.85f, 0.45f}) {}
 
+Screen& Renderer::getScreen() { return screen; }
+World& Renderer::getWorld() { return world; }
+
+void Renderer::renderSceneToScreen() {
+    for (auto& object : world.getObjects()) object->draw(world.getCamera(), screen);
+}
+
+void Renderer::renderScreenToFile(const std::string& file) const {
+    size_t width = screen.getWidth();
+    size_t height = screen.getHeight();
+
+    std::vector<uint8_t> pixels(width * height * 3, 0);
+
+    for (size_t x = 0; x < width; ++x) {
+        for (size_t y = 0; y < height; ++y) {
+            ColorType color = screen.getPixelColor(x, y);
+
+            pixels[3 * (y * width + x) + 0] = uint8_t(255.99f * color.r);
+            pixels[3 * (y * width + x) + 1] = uint8_t(255.99f * color.g);
+            pixels[3 * (y * width + x) + 2] = uint8_t(255.99f * color.b);
+        }
+    }
+
+    stbi_write_png(file.c_str(), width, height, 3, &pixels[0], width * 3);
+}
+
 }  // namespace eng
