b6d9fa6fe1d4c4fc4814de3675db78338149e348
Alexander Smorodinov <asmorodinov66@gmail.com>
2020-11-28 04:48:27 +0300

    Screen is no longer displayed upside-down, basic lighting

--- 3dRendererFromScratch/3dRendererFromScratch/main.cpp
+++ 3dRendererFromScratch/3dRendererFromScratch/main.cpp
@@ -55,32 +55,41 @@ int main() {
 
         auto& cam = renderer.getWorld().getCamera();
         cam.setPosition(5.5f * glm::vec3(std::cos(glm::radians(t / 3.0f)), 0.0f, std::sin(glm::radians(t / 3.0f))) +
-                        glm::vec3(0.0f, -0.5f + 0.2f * std::cos(glm::radians(t / 1.0f)), 0.0f));
+                        glm::vec3(0.0f, 1.5f + 0.8f * std::cos(glm::radians(t / 1.0f)), 0.0f));
         cam.setDirection(-cam.getPosition());
 
         auto& screen = renderer.getScreen();
         screen.setClearColor(clr * (2.0f + 0.2f * std::sin(glm::radians(t))));
 
         float s = 1.6f;
-        float h = 0.8;
+        float h = -0.8;
         objects.emplace_back(
-            std::make_unique<eng::Triangle>(glm::vec3(-s, h, s), glm::vec3(s, h, s), glm::vec3(-s, h, -s), eng::ColorType(1.0f)));
-        objects.emplace_back(std::make_unique<eng::Triangle>(glm::vec3(s, h, -s), glm::vec3(-s, h, -s), glm::vec3(s, h, s),
+            std::make_unique<eng::Triangle>(glm::vec3(-s, h, s), glm::vec3(-s, h, -s), glm::vec3(s, h, s), eng::ColorType(1.0f)));
+        objects.emplace_back(std::make_unique<eng::Triangle>(glm::vec3(s, h, -s), glm::vec3(s, h, s), glm::vec3(-s, h, -s),
                                                              eng::ColorType(1.0f), true));
 
         float sc = 1.1f;
         for (int z = -1; z <= 1; ++z)
             for (int x = -1; x <= 1; ++x)
                 for (int y = 0; y <= 0; ++y) {
+                    glm::vec3 ax = glm::vec3(1.0f * (x + 2), 0.8f * (y + 2), 0.7f * (z + 2));
+                    glm::vec3 pos = sc * glm::vec3(x, y, z);
+                    float scale = 0.35f;
+
+                    if (z == 0 && x == 0) {
+                        pos = {2, 1, 0};
+                        scale = 0.05f;
+                    }
+
                     glm::mat4 model = glm::mat4(1.0f);
-                    model = glm::translate(model, sc * glm::vec3(x, y, z));
-                    model = glm::rotate(model, glm::radians(t), glm::vec3(1.0f * (x + 2), 0.8f * (y + 2), 0.7f * (z + 2)));
-                    model = glm::scale(model, glm::vec3(0.35f));
+                    model = glm::translate(model, pos);
+                    model = glm::rotate(model, glm::radians(t), ax);
+                    model = glm::scale(model, glm::vec3(scale));
 
                     glm::mat4 model2 = glm::mat4(1.0f);
-                    model2 = glm::translate(model2, sc * glm::vec3(x, y, z));
-                    model2 = glm::rotate(model2, glm::radians(t), glm::vec3(1.0f * (x + 2), 0.8f * (y + 2), 0.7f * (z + 2)));
-                    model2 = glm::scale(model2, -glm::vec3(0.35f));
+                    model2 = glm::translate(model2, pos);
+                    model2 = glm::rotate(model2, glm::radians(t), ax);
+                    model2 = glm::scale(model2, -glm::vec3(scale));
 
                     std::vector<glm::vec3> points = {{1, 1, 1}, {-1, -1, 1}, {-1, 1, -1}, {1, -1, -1}};
                     for (auto& p : points) p = model * glm::vec4(p, 1.0f);
