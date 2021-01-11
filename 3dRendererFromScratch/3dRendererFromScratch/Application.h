c08700db5096ce83cf52922b7ad5bcce024b55f8
Alexander Smorodinov <asmorodinov66@gmail.com>
2021-01-12 02:21:28 +0300

    moved code from main.cpp to new class

---
+++ 3dRendererFromScratch/3dRendererFromScratch/Application.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <array>
+#include <string>
+#include <vector>
+#include <thread>
+#include <chrono>
+
+#include "Engine/Renderer.h"
+
+class Application {
+ public:
+    Application();
+    void initInterface();
+    void initShaders();
+    void addObjects();
+
+    void processEvents();
+    void run();
+
+ private:
+    size_t width;
+    size_t height;
+    sf::RenderWindow window;
+    eng::Renderer renderer;
+
+    eng::RenderMode rm;
+
+    int frames = 0;
+    float lastTime = 0.0f;
+    float fps = 0.0;
+
+    eng::Shader flatShader;
+    eng::Shader textureShader;
+    eng::Shader uvShader;
+    eng::Shader normalShader;
+    eng::Shader phongShader;
+
+    eng::Texture texture = eng::Texture("data/texture.png");
+    eng::Texture texture2 = eng::Texture("data/texture2.png");
+    eng::Texture texture3 = eng::Texture("data/texture3.png");
+    eng::Texture swordTexture = eng::Texture("data/sword.png");
+
+    sf::Font font;
+    sf::Text text;
+    sf::Text text2;
+    sf::Text text3;
+};
