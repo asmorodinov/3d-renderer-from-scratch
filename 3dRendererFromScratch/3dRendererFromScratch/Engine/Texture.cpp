e7d67681f2ef7ec01c9f938fd926132967f8313a
Alexander Smorodinov <asmorodinov66@gmail.com>
2021-01-12 03:07:13 +0300

    removed "hack" in Texture sample method

--- 3dRendererFromScratch/3dRendererFromScratch/Engine/Texture.cpp
+++ 3dRendererFromScratch/3dRendererFromScratch/Engine/Texture.cpp
@@ -25,12 +25,8 @@ Texture::Texture(const std::string& file) {
 }
 
 glm::vec4 Texture::sample(float x, float y, int n, int m) const {
-    // temporary hack, this will be removed when I add triangle clipping with frustrum planes
-    // (e.g. to not render objects behind camera)
-
-    // I think it can be removed, but let's leave it for now
     static constexpr float eps = 0.01f;
-    if (x < -eps || y < -eps || x > 1 + eps || y > 1 + eps) return glm::vec4();
+    assert(x > -eps && y > -eps && x < 1 + eps && y < 1 + eps);
 
     x = (x * n) - int(x * n);
     y = (y * m) - int(y * m);
